79da3cce33c2dbea2cb4c33bcd9c03a5e3d8ed06
Cadu Castanha <caducastanha@gmail.com>
2020-05-28 21:20:03 -0300

    CRUD com MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
-
+import { MongooseModule } from '@nestjs/mongoose'
 @Module({
-  imports: [TasksModule],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://cadu:cadu3105@cluster0-vjqze.mongodb.net/test?retryWrites=true&w=majority'),
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/tasks/shared/task.service.ts
+++ src/tasks/shared/task.service.ts
@@ -1,53 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { Task } from './task';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class TaskService {
-  tasks: Task[] = [
-    { id: 1, description: 'Tarefa 1', completed: false },
-    { id: 2, description: 'Tarefa 2', completed: true },
-    { id: 3, description: 'Tarefa 3', completed: false },
-    { id: 4, description: 'Tarefa 4', completed: true },
-    { id: 5, description: 'Tarefa 5', completed: false },
-    { id: 6, description: 'Tarefa 6', completed: false },
-    { id: 7, description: 'Tarefa 7', completed: false },
-    { id: 8, description: 'Tarefa 8', completed: false },
-  ];
 
-  getAll() {
-    return this.tasks;
+  constructor(@InjectModel('Task') private readonly taskModel: Model<Task>) { }
+
+  async getAll() {
+    return await this.taskModel.find().exec();
   }
 
-  getById(id: number) {
-    return this.tasks.find(task => task.id == id);
+  async getById(id: string) {
+    return await this.taskModel.findById(id).exec();
   }
 
-  create(task: Task) {
-    let lastId = 0;
-
-    if (this.tasks.length > 0) {
-      lastId = this.tasks[this.tasks.length - 1].id;
-    }
-    task.id = lastId + 1;
-
-    this.tasks.push(task);
-
-    return task;
+  async create(task: Task) {
+    const createTask = new this.taskModel(task);
+    return await createTask.save();
   }
 
-  update(task: Task) {
-    const oldTask = this.getById(task.id);
-
-    if (oldTask) {
-      oldTask.description = task.description;
-      oldTask.completed = task.completed;
-    }
-    return oldTask;
+  async update(id: string, task: Task) {
+    await this.taskModel.updateOne({ _id: id }, task).exec();
+    return this.getById(id);
   }
 
-  delete(id: number) {
-    const index = this.tasks.findIndex(task => task.id == id);
-    this.tasks.splice(index, 1);
+  async delete(id: string) {
+    await this.taskModel.deleteOne({ _id: id }).exec();
     return;
   }
 }
